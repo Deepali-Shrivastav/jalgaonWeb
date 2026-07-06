19894499ab207bbdf24ef061bd5f2ec4f92099f4
Rohan Phegade <130360879+Rohanphegade@users.noreply.github.com>
2026-07-06 13:55:56 +0530

    cicd

--- New-JalgaonUI/src/app/jobs/JobsPortal.tsx
+++ New-JalgaonUI/src/app/jobs/JobsPortal.tsx
@@ -268,11 +268,11 @@ export default function JobsPortal() {
                     >
                       <div className="flex gap-4 items-start">
                         <div className="w-12 h-12 rounded-lg bg-white flex items-center justify-center p-2 border border-amber-100 shrink-0">
-                          {job.logo ? (
+                          {job.company_logo ? (
                             <img
                               className="w-full h-full object-contain"
-                              src={job.logo}
-                              alt={job.logoAlt || job.company}
+                              src={job.company_logo}
+                              alt={job.company}
                               loading="lazy"
                             />
                           ) : (
