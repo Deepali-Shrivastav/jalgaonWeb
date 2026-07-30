8a3616a7d777c64833f710cc45f37c70ad492dc0
Harshal-Avate <harshalavate05@gmail.com>
2026-07-30 16:37:17 +0530

    feat: implement 410 Gone routes for SEO spam cleanup and optimize backend

---
+++ New-JalgaonUI/src/app/onlinets/[[...slug]]/route.ts
@@ -0,0 +1,9 @@
+import { gone } from "@/lib/spam-handler";
+
+export const GET = gone;
+export const HEAD = gone;
+export const POST = gone;
+export const PUT = gone;
+export const PATCH = gone;
+export const DELETE = gone;
+export const OPTIONS = gone;
